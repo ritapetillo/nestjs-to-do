8526ccbb51272df1d37cf3ea76621df07fbe4c4d
ritapetillo <rita.petillo@gmail.com>
2022-09-12 17:21:11 -0400

    boilerplate

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    console.log(title);
+
+    this.tasks.push(task);
+
+    return task;
+  }
+}
